79bc909f17a7311ffa3cbc06dc52f26237f8b2e0
Dim <dimggyt@gmail.com>
2017-11-14 17:23:50 -0500

    indentation fix

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 const extract = require('..');
 const images = {
-  Imgur: 'https://imgur.com/EQFdt2W',
-  Snaggy: 'https://snag.gy/7AyQk8.jpg',
-  Pasteboard: 'https://pasteboard.co/GRnUSdqN.png',
-  IMG42: 'https://img42.com/bJ59b',
-  Gyazo: 'https://gyazo.com/9a0187b816def3ea0b0451820318cecd',
+	Imgur: 'https://imgur.com/EQFdt2W',
+	Snaggy: 'https://snag.gy/7AyQk8.jpg',
+	Pasteboard: 'https://pasteboard.co/GRnUSdqN.png',
+	IMG42: 'https://img42.com/bJ59b',
+	Gyazo: 'https://gyazo.com/9a0187b816def3ea0b0451820318cecd',
 	LightShot: 'https://prnt.sc/h438tq',
 	DeviantArt: 'https://eddsworld.deviantart.com/art/EWCOMIC-No-218-Pit-679285783'
 };
@@ -12,12 +12,12 @@ const images = {
 process.on('unhandledRejection', console.error);
 
 (async () => {
-  for (const host in images) {
-    console.log([
-      `Host: ${host}`,
-      `  Source: ${images[host]}`,
-      `  Extracted: ${await extract(images[host])}`,
-      ''
-    ].join('\n'));
-  }
+	for (const host in images) {
+		console.log([
+			`Host: ${host}`,
+			`  Source: ${images[host]}`,
+			`  Extracted: ${await extract(images[host])}`,
+			''
+		].join('\n'));
+	}
 })();
